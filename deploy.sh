efb16ccbef2db8bb981ef22191273399d4bf6c14
asantesylvesterkwame <intelligentson17@gmail.com>
2025-08-16 23:49:25 +0000

    Update deployment script to fetch and reset to the latest master branch instead of main for consistency with repository structure.

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@
 cd /home/corpland/id.api.corplandtechnologies.com || exit
 
 # Reset to latest GitHub main branch
-git fetch origin main
-git reset --hard origin/main
+git fetch origin master
+git reset --hard origin/master
 
 # (Optional) If Node.js / backend project, install dependencies & restart process
 yarn install
